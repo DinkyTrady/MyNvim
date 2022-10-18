fe7ce9d8892d80838ff9498530011bbef322a3f2
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-18 23:41:11 +0700

    Add files via upload

--- lua/randy/cmp.lua
+++ lua/randy/cmp.lua
@@ -1,9 +1,20 @@
--- local has_words_before = function()
+  -- local has_words_before = function()
   --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  -- end                                                                                                                                                                                                                                                                                                                                                                                                                                    local cmp = require'cmp'
-  local luasnip = require'luasnip'                                                                                                                                                                                     local lspkind = require('lspkind')
-  require("bufferline").setup{}                                                                                                                                                                                        cmp.setup({                                                                                                                                                                                                            snippet = {                                                                                                                                                                                                            -- REQUIRED - you must specify a snippet engine                                                                                                                                                                      expand = function(args)                                                                                                                                                                                                --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.                                                                                                                                                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.                                                                                                                                                     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.                                                                                                                                                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+  -- end
+
+  local cmp = require'cmp'
+  local luasnip = require'luasnip'
+  local lspkind = require('lspkind')
+  require("bufferline").setup{}
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
     window = {
--- lua/randy/lspconfig.lua
+++ lua/randy/lspconfig.lua
@@ -1,14 +1,14 @@
 -- Setup lspconfig.
 local nvim_lsp = require('lspconfig')
 local server = { 'tsserver', 'html', 'cssls', 'emmet_ls', 'clangd' }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.ls
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _,lsp in ipairs(server) do
   nvim_lsp[lsp].setup {
     capabilities = capabilities,
   }
 end
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
