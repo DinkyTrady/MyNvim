ea3717faf4cbdf6b71925d41ac61ff1d3e9778d6
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-18 23:39:36 +0700

    Add files via upload

---
+++ lua/randy/cmp.lua
@@ -0,0 +1,92 @@
+-- local has_words_before = function()
+  --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  -- end                                                                                                                                                                                                                                                                                                                                                                                                                                    local cmp = require'cmp'
+  local luasnip = require'luasnip'                                                                                                                                                                                     local lspkind = require('lspkind')
+  require("bufferline").setup{}                                                                                                                                                                                        cmp.setup({                                                                                                                                                                                                            snippet = {                                                                                                                                                                                                            -- REQUIRED - you must specify a snippet engine                                                                                                                                                                      expand = function(args)                                                                                                                                                                                                --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.                                                                                                                                                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.                                                                                                                                                     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.                                                                                                                                                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered()
+    },
+    formatting = {
+      format = function(entry, vim_item)
+        vim_item.abbr = string.sub(vim_item.abbr, 1, 40)
+        return vim_item
+      end,
+      lspkind.cmp_format({
+        mode = "symbol_text",
+        menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[Latex]",
+        })
+      }),
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<Down>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        -- elseif luasnip.expand_or_jumpable() then
+        --   luasnip.expand_or_jump()
+        -- elseif has_words_before() then
+        --   cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<Up>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      --{ name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.                                                                        }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
