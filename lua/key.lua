743231eb3ef9c15b8d2e328c6e222b501bcf1930
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-19 21:07:00 +0700

    Update key.lua

--- lua/key.lua
+++ lua/key.lua
@@ -1,7 +1,13 @@
+--for easy
 local o = vim.opt
+local opts = { noremap = true }
+local maps = { noremap = true, silent = true }
+--seting
 o.termguicolors=true
 o.number=true
 o.expandtab=true
 o.shiftwidht=2
 o.tabstop=2
 o.relativenumber=true
+--mappings
+maps('n', '<C-n>' ":NvimTreeToggle<CR>", opts)
