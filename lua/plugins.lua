3ca4f42b8bb2afadb7ee0314a3634fa8f85bbdb3
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-19 20:48:37 +0700

    Create plugins.lua

---
+++ lua/plugins.lua
@@ -0,0 +1,26 @@
+--To install plugin use `:PlugInstall`
+--Untuk menginstall plugin ketik `:PlugInstall`
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '/.config/nvim/plugged')
+  Plug 'neovim/nvim-lspconfig'
+  Plug 'hrsh7th/cmp-nvim-lsp'
+  Plug 'hrsh7th/cmp-buffer'
+  Plug 'hrsh7th/cmp-path'
+  Plug 'hrsh7th/cmp-cmdline'
+  Plug 'hrsh7th/nvim-cmp'
+  Plug 'L3MON4D3/LuaSnip'
+  Plug 'saadparwaiz1/cmp_luasnip'
+  Plug 'rafamadriz/friendly-snippets'
+  Plug 'onsails/lspkind-nvim'
+  Plug 'numToStr/Comment.nvim'
+  Plug 'jiangmiao/auto-pairs'
+  Plug 'lukas-reineke/indent-blankline.nvim'
+  Plug ('akinsho/bufferline.nvim', { 'tag': 'v2.*' })
+  Plug 'kyazdani42/nvim-web-devicons'
+  Plug 'kyazdani42/nvim-tree.lua'
+  Plug 'nvim-lualine/lualine.nvim'
+  Plug ('folke/tokyonight.nvim', { 'branch': 'main' })
+  Plug ('nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'})
+vim.call('plug#end')
+--Colorscheme
+vim.cmd('colorscheme tokyonight-night')
