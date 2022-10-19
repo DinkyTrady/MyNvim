836dcbcf9608c7c37132970a2ef403894d0a6816
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-19 07:02:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ npm install -g emmet-ls
 npm insatll -g live-server
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-git clone https://github.com/DinkyTrady/My-Neovim-Setup ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/DinkyTrady/My-Neovim-Setup ~/.config/nvim --depth 1 && nvim :PlugInstall
