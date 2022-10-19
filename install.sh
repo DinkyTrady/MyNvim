2ddb67ecb56bd775498f12a89e154a7753ffbc1e
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-19 19:45:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pkg install mc neovim-nightly nodejs clang -y && npm i -g vscode-langservers-extracted emmet-ls live-server typescript-language-server && mv font.ttf ../.termux/ && termux-reload-settings
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-git clone https://github.com/DinkyTrady/My-Neovim-Setup ~/.config/nvim --depth 1 && cd && cd .config/nvim && nvim init.vim
+cd && cd .config/nvim && nvim init.vim
