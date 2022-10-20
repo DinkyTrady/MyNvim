86d0cd4869fb4b739682a1fc4b4081f11c45b624
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-20 22:13:31 +0700

    Update lsp.lua

--- lua/Plugins/lsp.lua
+++ lua/Plugins/lsp.lua
@@ -1,15 +1,19 @@
 -- Setup lspconfig.
+local mason = require("mason-lspconfig")
+mason.setup({
+  ensure_installed = { 'sumneko_lua', 'html', 'cssls', 'tsserver', 'emmet_ls' },
+  automatic_installation = true
+})
 local nvim_lsp = require('lspconfig')
-local server = { 'tsserver', 'html', 'cssls', 'emmet_ls', }
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-for _,lsp in ipairs(server) do
+for _,lsp in ipairs(mason.get_installed_servers()) do
   nvim_lsp[lsp].setup {
     capabilities = capabilities,
   }
-end
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
+  end
 end
