825a2e8f7c1d5783c4e128d94f622ac5798f43ab
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-20 11:24:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pkg install mc neovim-nightly nodejs clang -y && npm i -g vscode-langservers-extracted && npm install -g emmet-ls && npm install -g live-server && npm install -g typescript-language-server typescript
 sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim -es -u init.lua -i NONE -c "PlugInstall" -c "qa"
+nvim -es -u init.lua -i NONE -c "PlugUpdate" -c "qa"
