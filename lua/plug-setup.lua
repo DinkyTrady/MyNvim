48a9e2c10d9b32eae3931a52b49b987f940a41f4
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-20 12:48:10 +0700

    Update plug-setup.lua

--- lua/plug-setup.lua
+++ lua/plug-setup.lua
@@ -1,21 +1,13 @@
-require('randy.cmp')
-require('randy.lsp')
-require('set.nvim-tree')
-require('set.line')
-require('set.cs')
-require('set.key')
---indent-blankline
-vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
-
-require("indent_blankline").setup {
-    show_end_of_line = true,
-}
---bufferline
-require ("bufferline").setup{}
+require('Plugins.cmp')
+require('Plugins.lsp')
+require('Plugins.nvim-tree')
+require('Plugins.lualine')
+require('Plugins.tokyonight')
+require('Plugins.blankline')
+require('Plugin.bufferline')
 --snip
 require("luasnip.loaders.from_vscode").lazy_load()
 --autocomments
 require('Comment').setup()
---nvim-tree-setup↴
+--nvim-treesitter-setup↴
 require'nvim-treesitter.configs'.setup{highlight={enable=true}}
