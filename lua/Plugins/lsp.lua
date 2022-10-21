d250de05a258c502555d7ae5afdb0dfe0d148f00
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2022-10-21 12:43:52 +0700

    Update lsp.lua

--- lua/Plugins/lsp.lua
+++ lua/Plugins/lsp.lua
@@ -11,9 +11,13 @@ for _,lsp in ipairs(mason.get_installed_servers()) do
   nvim_lsp[lsp].setup {
     capabilities = capabilities,
   }
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
-  end
+end
+nvim_lsp.clangd.setup {
+  capabilities = capabilities
+}
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
